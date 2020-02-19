619a7302880ea95e4575836dc786fe72d8997f08
Riska <handayaniriska762@gmail.com>
2020-02-19 12:27:25 +0700

    okey

--- creatingreact/src/layout/about.js
+++ creatingreact/src/layout/about.js
@@ -1,7 +1,9 @@
 import React from "react";
+import mainLogo from "../userProfile/fisika.jpg";
 class About extends React.Component {
   render = () => (
     <div className="container mt-2">
+      <img src={mainLogo} alt="tokopedia" />
       <h1>Fun Physics</h1>
       <ul>
         <li>
--- creatingreact/src/userProfile/avatar.js
+++ creatingreact/src/userProfile/avatar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import mainLogo from "./tokopedia.jpg";
-
+import "../userProfile/profile.css";
 class Avatar extends React.Component {
   render() {
     return (
