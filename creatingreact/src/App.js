9d72011be9f44bff4ec6495f51aaf19aafc5cc75
Riska <handayaniriska762@gmail.com>
2020-02-19 13:34:59 +0700

    hm

--- creatingreact/src/App.js
+++ creatingreact/src/App.js
@@ -1,28 +1,6 @@
 import React from "react";
 import "./App.css";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Multiple from "./form/multipleform";
-// import Home from "./layout/home";
-// import Profile from "./userProfile/profile";
-// import About from "./layout/about";
-// import Main from "./layout/main";
-// const App = props => {
-//   return (
-//     <Router>
-//       <Switch>
-//         <Main>
-//           <Switch>
-//             <Route exact path="/" component={Home} />
-//             <Route path="/home" component={Home} />
-//             <Route path="/profile" component={Profile} />
-//             <Route path="/about" component={About} />
-//           </Switch>
-//         </Main>
-//       </Switch>
-//     </Router>
-//   );
-// };
-// export default App;
 const App = props => {
   return (
     <div>
