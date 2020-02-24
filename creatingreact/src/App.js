476bce438396e7c3d72ececf3c3ffc9fee4ec211
Riska <handayaniriska762@gmail.com>
2020-02-24 10:05:10 +0700

    use global hook

--- creatingreact/src/App.js
+++ creatingreact/src/App.js
@@ -1,30 +1,17 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./layout/main";
-import ID from "./api/getid";
-import GET from "./api/book";
-// import Books from "./api/GetUsingHook";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Main from "./layout/main";
+// import ID from "./api/getid";
+// import GET from "./api/book";
+// // import Books from "./api/GetUsingHook";
+// // import Update from "./api/Updatebooks";
+// // import Delete from "./api/Deletebooks";
 // import Update from "./api/Updatebooks";
-// import Delete from "./api/Deletebooks";
-import Update from "./api/Updatebooks";
-import Post from "./api/Postbooks";
+// import Post from "./api/Postbooks";
+import Index from "./index";
 
 const App = props => {
-  return (
-    <Router>
-      <Switch>
-        <Main>
-          <Switch>
-            <Route path="/get/books/" component={GET} />
-            <Route path="/get/books/:id" component={ID} />
-            <Route path="/post/books" component={Post} />
-            <Route path="/update/books/:id" component={Update} />
-            {/* <Route path="/delete/books/:id" component={Delete} /> */}
-          </Switch>
-        </Main>
-      </Switch>
-    </Router>
-  );
+  return <Index />;
 };
 export default App;
