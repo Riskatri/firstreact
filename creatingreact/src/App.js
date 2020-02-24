07604bb50feb09d991e9a98672fa8d6d732304ca
Riska <handayaniriska762@gmail.com>
2020-02-24 10:06:02 +0700

    first

--- creatingreact/src/App.js
+++ creatingreact/src/App.js
@@ -1,14 +1,5 @@
 import React from "react";
 import "./App.css";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Main from "./layout/main";
-// import ID from "./api/getid";
-// import GET from "./api/book";
-// // import Books from "./api/GetUsingHook";
-// // import Update from "./api/Updatebooks";
-// // import Delete from "./api/Deletebooks";
-// import Update from "./api/Updatebooks";
-// import Post from "./api/Postbooks";
 import Index from "./index";
 
 const App = props => {
